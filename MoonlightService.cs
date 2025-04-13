cb1496ee2bffa2743e84a761ae9c919d93c56fd1
kaenguruu <user.jakob@outlook.de>
2025-04-13 15:37:32 +0200

    Added auth

--- MoonlightService.cs
+++ MoonlightService.cs
@@ -1,5 +1,9 @@
 using Docker.DotNet;
 using Docker.DotNet.Models;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
 
 namespace MoonlightToCloudflareDNS;
 
@@ -9,6 +13,7 @@ public static class MoonlightService
 		new DockerClientConfiguration(new Uri("unix:///var/run/docker.sock")).CreateClient();
 
 	private static string? _apiKey;
+	private static readonly List<Server> Servers = [];
 
 	public static async Task Run(Dictionary<string, string?> configuration)
 	{
@@ -21,7 +26,18 @@ public static class MoonlightService
 			return;
 		}
 
+		var builder = WebApplication.CreateBuilder();
+		var app = builder.Build();
+		app.Urls.Add("http://localhost:5000");
+
+		app.MapGet("/servers", (HttpContext context) =>
+		{
+			var authHeader = context.Request.Headers.Authorization.ToString();
+			return authHeader != $"Bearer {_apiKey}" ? Results.Unauthorized() : Results.Ok(Servers);
+		});
+
 		await Logging.Log(LogSeverity.Info, "Moonlight", "Configuration is valid. Monitoring...");
+		_ = app.RunAsync();
 		await MonitorForChanges();
 	}
 
@@ -33,15 +49,36 @@ public static class MoonlightService
 			if (containers.Count <= 0)
 				continue;
 
+			Servers.Clear();
 			foreach (var container in containers)
 			{
 				var name = container.Names.FirstOrDefault(n => n.Contains("/"))?.Replace("/", "");
-				
-				
+
 				if (name == null || !name.Contains("moonlight-runtime-"))
 					continue;
 
-				Console.WriteLine(container.Mounts.FirstOrDefault()?.Source);
+				if (container.Mounts.FirstOrDefault()?.Source == null)
+					continue;
+
+				const string relativeConfigFilePath = "/dns.json";
+				if (!File.Exists(container.Mounts.FirstOrDefault()?.Source + relativeConfigFilePath))
+					continue;
+
+				try
+				{
+					var json = await File.ReadAllTextAsync(container.Mounts.FirstOrDefault()?.Source +
+					                                       relativeConfigFilePath);
+					var server = JsonConvert.DeserializeObject<Server>(json);
+					if (server == null)
+						continue;
+
+					Servers.Add(server);
+				}
+				catch (Exception ex)
+				{
+					await Logging.Log(LogSeverity.Error, "ML/Monitor",
+						$"Invalid DNS config for: {container.Names.FirstOrDefault()} [{ex.Message}]");
+				}
 			}
 
 			await Task.Delay(30_000);
